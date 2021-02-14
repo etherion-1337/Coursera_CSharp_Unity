4c0731951dd1fd9f34f1acd177c6e9ef6df69a26
etherion <46531622+etherion-1337@users.noreply.github.com>
2021-02-14 23:13:02 +0800

    week 1 start

---
+++ Course_1/week_1/FirstConsoleApp/FirstConsoleApp/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Print a message
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // print supportive message
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+        }
+    }
+}
